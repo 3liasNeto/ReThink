1047ae6a970168ee5d6ea3ebf77107040c8dc1fd
3lDarky <3lias.neto@gmail.com>
2023-01-31 10:59:28 -0300

    reThink 0.0.2

--- ReThink/site/js/text-themes.js
+++ ReThink/site/js/text-themes.js
@@ -1,20 +1,40 @@
-function fetchApi(){
-fetch("https://pensador-api.vercel.app/?term=Jesus+Cristo&max=7")
-.then((response) => response.json())
-.then(data => { 
-    const div = document.querySelector('.div-text_reT');
-    data.map((item)=>{
-        const author = document.createElement('h1');
-        const text = document.createElement('h1');
+const autor = document.querySelector('.h-author');
+const frase = document.querySelector('.h-article');
+// text effect 
+var i = 0;
+var html = frase.innerHTML;
+var attr = frase.setAttribute("data", html);
+var txt = frase.getAttribute("data");
+var speed = 400;
 
-        author.setAttribute('class', 'h-author');
-        text.setAttribute('class', 'h-article');
-
-        author.innerHTML = item.autor;
-        text.innerHTML = item.texto;
-
-        div.appendChild(text);
-        div.appendChild(author);
-    })
-});
+function typeWriter() {
+  if (i <= txt.length) {
+    document.querySelector(".h-article").innerHTML = txt.slice(0 , i + 1);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 }
+
+typeWriter();
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+const randonPhrase = randomInt(0, 10);
+const termo = ["Friedrich Nietzsche","Fernando Pessoa",]
+
+const url = `https://pensador-api.vercel.app/?term=${termo}`
+fetch(url)
+    .then(response => response.json())
+    .then(result => {
+        autor.innerText = result.frases[randonPhrase].autor
+        frase.innerText = result.frases[randonPhrase].texto
+    })
+    .catch(error => console.log('error', error));
+
+
+
+
+// acessibilidade movel, nao repetir a mesma frase, limite de frase por dia, e atualização 24h
